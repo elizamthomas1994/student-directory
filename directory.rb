32b04d1c0486f32df18c975da84941e681a6d9b5
Eliza Thomas <elizamthomas1994@gmail.com>
2022-10-31 13:27:46 +0000

    Refactored code and made it easier to read

--- directory.rb
+++ directory.rb
@@ -1,39 +1,46 @@
+@students = []
+
 def interactive_menu
-  students = []
   loop do
-  #1. Print the menu and ask the user what to do
+  print_menu
+  process(gets.chomp)
+  end
+end
+
+def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "9. Exit"
-  #2. Read the input and save it into a variable
-  selection = gets.chomp
-  #3. Do what the user has asked
+end
+  
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
   case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
-      exit # this will cause the program to terminate
+      exit
     else
       puts "I don't know what you meant, try again"
-    end
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
 end
 
 #Then, we print a list of students using the print_header
@@ -43,15 +50,15 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 #Then, we print the number of students.
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
